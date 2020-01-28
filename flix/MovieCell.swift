db0921b1d34718e9e334c66ed690134d782c9e0b
Sean-HU <32807155+Sean-HU@users.noreply.github.com>
2020-01-27 21:15:18 -0500

    Flix app

---
+++ flix/MovieCell.swift
@@ -0,0 +1,28 @@
+//
+//  MovieCell.swift
+//  flix
+//
+//  Created by Sean Mills on 1/27/20.
+//  Copyright © 2020 Sean Mills. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    @IBOutlet weak var posterView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
