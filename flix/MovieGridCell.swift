cfff8aad38ae1298ab6246efb8d745873627c183
Sean-HU <32807155+Sean-HU@users.noreply.github.com>
2020-02-10 21:53:14 -0500

    Flix Part 2

---
+++ flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flix
+//
+//  Created by Sean Mills on 2/10/20.
+//  Copyright © 2020 Sean Mills. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
